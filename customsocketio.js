52af356228c5a7f5c81b6a9988d396bdcce180ab
John Hiott <jhiott@oolcay.com>
2014-10-28 14:01:56 -0400

    error checking

--- customsocketio.js
+++ customsocketio.js
@@ -14,16 +14,44 @@ $( document ).ready(function(){
     min = $('#factorymin').val();
     max = $('#factorymax').val();
 
+    errors = [];
+    failed = false;
+
     //TODO: do more with invalid input
+    if (name && name.length > 100){
+      errors.push("Name is to long, must be less than < 100 chars");
+      failed = true;
+    }
     if (min < 1){  //min must be greater than one
-      return;
+      errors.push("Min must be greate than 1");
+      failed = true;
     }
     if (max <= min ){  //max must be more than min
-      return;
+      errors.push("max must be more than min");
+      failed = true;
     }
     if (name.length < 2){  //name must be two chars long
+      errors.push("Name must be > than 2 characters");
+      failed = true;
+    }
+    if (max > 100000){
+      errors.push("Max must be less than 100,000")
+      failed = true;
+    }
+
+    if (failed){
+      errorsDiv =  $('#errors');
+      errorsDiv.show();
+      errorHtml = "";
+      for (x=0; x<errors.length; x++){
+        errorHtml += errors[x] + '<br/>';
+      }
+
+      errorsDiv.html(errorHtml);
       return;
     }
+
+    $('#errors').hide();
 
     $(this).prop("disabled", true);  //disable save button, enable again on success or fail of save
     socket.emit('createfactory', { name: name, min: min, max: max } );
@@ -48,6 +76,13 @@ $( document ).ready(function(){
   $('#generateworkers').click(function(){
     quantity = $('#workerquantity').val();
 
+    if ( quantity < 1 || quantity >  15 ){
+      $('#errors').show();
+      $('#errors').html("Must be between 1 and 15");
+      return;
+    }
+
+    $('#errors').hide();
     instance = $('#jstree').jstree(true);
     id = instance.get_selected (false);
     msg = {
